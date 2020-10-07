2748fd24ea962fed61e73cae80ec09d6d10d4188
Lukas Landgraf <llandgraf317@gmail.com>
2020-10-07 15:13:54 +0200

    Naming of NVM directories and AccessManager

--- include/core/access/root.h
+++ include/core/access/root.h
@@ -47,8 +47,9 @@ class RootInitializer {
     std::string m_LayoutName;
     std::string m_FileName;
     uint64_t m_PoolSize;
-    //static constexpr auto LAYOUT = "NVMDS";
     static constexpr uint64_t POOL_SIZE = 1024 * 1024 * 1024ul * ENV_POOL_SIZE;  //< 4GB
+    const std::string m_PmemPath = "/mnt/pmem";
+    const std::string m_DirName = "morphstore/";
 
     std::vector<bool> m_ReadSuccessful;
 
@@ -73,8 +74,9 @@ public:
     root_retrieval getPoolRoot(int pmemNode)
     {
         root_retrieval retr;
-        std::string path = (pmemNode == 0 ? gPmemPath0 : gPmemPath1) + m_FileName;
-        const std::string& gPmem = (pmemNode == 0 ? gPmemPath0 : gPmemPath1);
+
+        std::string path = getDirectory(pmemNode) + m_FileName;
+        const std::string& gPmem = getDirectory(pmemNode);
 
         if (access(path.c_str(), F_OK) != 0) {
             mkdir(gPmem.c_str(), 0777);
@@ -100,13 +102,16 @@ public:
         return retr;
     }
 
+    std::string getDirectory(size_t numaNode)
+    {
+        return m_PmemPath + std::to_string(numaNode) + "/" + m_DirName;
+    }
+
     void cleanUp()
     {
-        std::string path0 = gPmemPath0 + m_FileName;
-        std::string path1 = gPmemPath1 + m_FileName;
+        for (uint64_t node = 0; node < getNumaNodeCount(); node++)
+            remove( (m_PmemPath + std::to_string(node) + "/morphstore/" + m_FileName).c_str() );
 
-        remove(path0.c_str());
-        remove(path1.c_str());
     }
 
     void initPmemPool(std::string filename, std::string layoutname, uint64_t poolSize = POOL_SIZE)
--- include/core/queryprocessing/AccessPathManager.h
+++ include/core/queryprocessing/AccessPathManager.h
@@ -1,8 +1,68 @@
 #pragma once
 
+#include <unistd.h>
+#include <numa.h>
+#include <sys/statvfs.h>
+
 namespace morphstore {
 
 class AccessPathManager {
+public:
+    static AccessPathManager & getInstance() {
+        static AccessPathManager instance;
+        return instance;
+    }
+
+    size_t getTotalSystemMemory()
+    {
+        long pages = sysconf(_SC_PHYS_PAGE);
+        long page_size = sysconf(_SC_PAGE_SIZE);
+        return pages * page_size;
+    }
+
+    size_t nvramRemaining(size_t nodeNumber)
+    {
+
+    }
+
+    size_t dramRemaining(size_t nodeNumber)
+    {
+
+    }
+
+    void pinExecutionToNode(size_t nodeNumber)
+    {
+
+    }
+
+    bool checkNumaLocation(void* address, size_t nodeNumber)
+    {
+
+    }
+
+    column<uncompr_f> * optimizeBetween(column<uncompr_f> * col, uint64_t lower, uint64_t upper)
+    {
+
+        return nullptr;
+    }
+
+    column<uncompr_f> * optimizeSelect(column<uncompr_f> * col, uint64_t pred)
+    {
+
+        return nullptr;
+    }
+
+    column<uncompr_f> * optimizeGroupAggr(column<uncompr_f> * grCol, column<uncompr_f> * dataCol)
+    {
+
+        return nullptr;
+    }
+
+    column<uncompr_f> * optimizeJoin(column<uncompr_f> * left, column<uncompr_f> * right)
+    {
+
+        return nullptr;
+    }
 
 }
 
