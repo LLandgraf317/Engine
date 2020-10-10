ba0141259ba0fe4451a714d05b1feea02a107390
Lukas Landgraf <llandgraf317@gmail.com>
2020-10-10 12:58:29 +0200

    Assertions about NUMA locality

--- src/microbenchmarks/nvmds_bench.cc
+++ src/microbenchmarks/nvmds_bench.cc
@@ -456,15 +456,15 @@ int main(int /*argc*/, char** /*argv*/)
 
     // Checks for numa location property
     for (uint64_t i = 0; i < node_number; i++) {
-        repl_mgr.isLocOnNode(primColNode[i]->get_data(), i);
-        repl_mgr.isLocOnNode(valColNode[i]->get_data(), i);
+        assert(repl_mgr.isLocOnNode(primColNode[i]->get_data(), i));
+        assert(repl_mgr.isLocOnNode(valColNode[i]->get_data(), i));
 
-        repl_mgr.isLocOnNode(primColPersConv[i]->get_data(), i);
-        repl_mgr.isLocOnNode(valColPersConv[i]->get_data(), i);
+        assert(repl_mgr.isLocOnNode(primColPersConv[i]->get_data(), i));
+        assert(repl_mgr.isLocOnNode(valColPersConv[i]->get_data(), i));
 
-        repl_mgr.isLocOnNode(trees[i].get(), i);
-        repl_mgr.isLocOnNode(skiplists[i].get(), i);
-        repl_mgr.isLocOnNode(hashmaps[i].get(), i);
+        assert(repl_mgr.isLocOnNode(trees[i].get(), i));
+        assert(repl_mgr.isLocOnNode(skiplists[i].get(), i));
+        assert(repl_mgr.isLocOnNode(hashmaps[i].get(), i));
     }
 
     // Benchmark: sequential insertion
