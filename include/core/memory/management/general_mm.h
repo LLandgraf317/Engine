8e31b46cfc5ec2ba78be23fff9a1ab52f2b8b4df
Lukas Landgraf <llandgraf317@gmail.com>
2020-10-10 16:06:09 +0200

    Added further debugging information to general_mm.h

--- include/core/memory/management/general_mm.h
+++ include/core/memory/management/general_mm.h
@@ -128,8 +128,9 @@ class general_memory_manager : public abstract_memory_manager {
       void* allocateNuma(size_t p_AllocSize, int numa_node) {
          void* ptr = numa_alloc_onnode(p_AllocSize, numa_node);
          int status = 0;
-         numa_move_pages( 0 /*calling process this*/, 1 /* we dont move pages */, reinterpret_cast<void**>(ptr), nullptr, &status, MPOL_MF_MOVE);
-         trace_l(T_DEBUG, "Tried to alloc on node ", numa_node, ", allocated on ", status);
+         errno = 0;
+         auto ret = numa_move_pages( 0 /*calling process this*/, 1 /* we dont move pages */, reinterpret_cast<void**>(ptr), nullptr, &status, MPOL_MF_MOVE);
+         trace_l(T_DEBUG, "Tried to alloc on node ", numa_node, ", allocated on ", status, ", move_pages returned ", ret, ", errno is ", errno);
          assert(status == numa_node);
          return ptr;
       }
