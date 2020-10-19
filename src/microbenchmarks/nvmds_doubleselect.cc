ec9e0a80d684bf9fbf2db4a91f48e3e408812739
Lukas Landgraf <llandgraf317@gmail.com>
2020-10-19 12:58:15 +0200

    Completed benchmark creation for double select

--- src/microbenchmarks/nvmds_doubleselect.cc
+++ src/microbenchmarks/nvmds_doubleselect.cc
@@ -11,6 +11,7 @@
 
 #include <core/operators/general_vectorized/project_compr.h>
 #include <core/operators/general_vectorized/select_compr.h>
+#include <core/operators/general_vectorized/intersect_uncompr.h>
 #include <core/operators/scalar/agg_sum_uncompr.h>
 #include <core/operators/scalar/select_uncompr.h>
 
@@ -35,6 +36,7 @@ public:
 
     const unsigned MAX_SEL_X = 999;
     const unsigned MAX_SEL_Y = 10;
+    const unsigned MAX_SEL_Z = 10;
 
     void initData() {
         auto repl_mgr = ReplicationManager::getInstance();
@@ -125,39 +127,52 @@ public:
 
     // Select sum(x) from r where y = c and z = b
     template< typename index_structure_ptr >
-    void runIndex(const column<uncompr_f> * xCol, index_structure_ptr index, const uint64_t selection)
+    void runIndex(const column<uncompr_f> * xCol, index_structure_ptr yIndex, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2)
     {
         start();
         auto select1 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
             index_structure_ptr, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
-                ::apply( index, selection);
+                ::apply( yIndex, selection1);
         auto select2 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
-            index_structure_ptr, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
-                ::apply( index, selection);
-        auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, select);
+            persistent_ptr<MultiValTreeIndex>, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
+                ::apply( zTree, selection2);
+        auto intersect = intersect_sorted<ps, uncompr_f, uncompr_f, uncompr_f >(select1, select2);
+
+        auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, intersect);
 
         auto res = agg_sum<ps, uncompr_f>(projection);
         end();
         outCsv();
 
-        delete select;
+        delete select1;
+        delete select2;
+        delete intersect;
         delete projection;
         delete res;
     }
 
     template< typename col_ptr >
-    void runCol(const column<uncompr_f> * xCol, col_ptr col, const uint64_t selection)
+    void runCol(const column<uncompr_f> * xCol, col_ptr zCol, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2)
     {
         start();
-        auto select = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
-                ::apply( col, selection);
-        auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, select);
+        auto select1 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
+                ::apply( zCol, selection1);
+        /*auto select2 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
+                ::apply( col, selection2);*/
+        auto select2 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
+            persistent_ptr<MultiValTreeIndex>, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
+                ::apply( zTree, selection2);
+        auto intersect = intersect_sorted<ps, uncompr_f, uncompr_f, uncompr_f >(select1, select2);
+
+        auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, intersect);
         auto res = agg_sum<ps, uncompr_f>(projection);
 
         end();
         outCsv();
 
-        delete select;
+        delete select1;
+        delete select2;
+        delete intersect;
         delete projection;
         delete res;
     }
@@ -183,20 +198,22 @@ public:
             auto yPCol = yStatus->getPersistentColumn(node)->convert();
             auto yVCol = yStatus->getVColumn(node);
 
+            const uint64_t val2 = 2;
+
             for (size_t val = 1; val < MAX_SEL_Y; val++) {
                 printColumnSize();
                 comma();
                 printSelectivity(yTree, val);
                 comma();
-                runCol  (xCol, yVCol, val);
+                runCol  (xCol, yVCol, zTree, val, val2);
                 comma();
-                runCol  (xCol, yPCol, val);
+                runCol  (xCol, yPCol, zTree, val, val2);
                 comma();
-                runIndex(xCol, yTree, val); 
+                runIndex(xCol, yTree, zTree, val, val2); 
                 comma();
-                runIndex(xCol, yHash, val);
+                runIndex(xCol, yHash, zTree, val, val2);
                 comma();
-                runIndex(xCol, ySkip, val);
+                runIndex(xCol, ySkip, zTree, val, val2);
                 nextCsvRow();
             }
 
