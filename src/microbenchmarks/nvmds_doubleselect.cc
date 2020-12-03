d047d67df769f7965f216c1327fb5eb29ae1063f
Lukas Landgraf <llandgraf317@gmail.com>
2020-12-03 09:13:16 +0100

    Added variable selectivity of second selection param

--- src/microbenchmarks/nvmds_doubleselect.cc
+++ src/microbenchmarks/nvmds_doubleselect.cc
@@ -149,7 +149,7 @@ public:
 
     // Select sum(x) from r where y = c and z = b
     template< typename index_structure_ptr >
-    void runIndex(const column<uncompr_f> * xCol, index_structure_ptr yIndex, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2, size_t runnode1, size_t runnode2)
+    void runIndex(const column<uncompr_f> * xCol, index_structure_ptr yIndex, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2, size_t runnode1)
     {
         numa_run_on_node(runnode1);
         start();
@@ -161,7 +161,7 @@ public:
                 ::apply( zTree, selection2);
         auto intersect = intersect_sorted<ps, uncompr_f, uncompr_f, uncompr_f >(select1, select2);
 
-        numa_run_on_node(runnode2);
+        //numa_run_on_node(runnode2);
         auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, intersect);
         auto res = agg_sum<ps, uncompr_f>(projection);
         end();
@@ -175,12 +175,12 @@ public:
     }
 
     template< typename col_ptr >
-    void runCol(const column<uncompr_f> * xCol, col_ptr yCol, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2, size_t runnode1, size_t runnode2)
+    void runCol(const column<uncompr_f> * xCol, col_ptr yCol, persistent_ptr<MultiValTreeIndex> zTree, const uint64_t selection1, const uint64_t selection2, size_t runnode1)
     {
         numa_run_on_node(runnode1);
         start();
         auto select1 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
-                ::apply( zCol, selection1);
+                ::apply( yCol, selection1);
         /*auto select2 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
                 ::apply( col, selection2);*/
         auto select2 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
@@ -188,7 +188,7 @@ public:
                 ::apply( zTree, selection2);
         auto intersect = intersect_sorted<ps, uncompr_f, uncompr_f, uncompr_f >(select1, select2);
 
-        numa_run_on_node(runnode2);
+        //numa_run_on_node(runnode2);
         auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, intersect);
         auto res = agg_sum<ps, uncompr_f>(projection);
 
@@ -203,20 +203,22 @@ public:
     }
 
     template< typename col_ptr >
-    void runColCol(const column<uncompr_f> * xCol, col_ptr yCol, col_ptr yCol, const uint64_t selection1, const uint64_t selection2, size_t runnode1, size_t runnode2)
+    void runColCol(const column<uncompr_f> * xCol, col_ptr yCol, col_ptr zCol, const uint64_t selection1, const uint64_t selection2, size_t runnode1)
     {
         numa_run_on_node(runnode1);
         start();
         auto select1 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
-                ::apply( zCol, selection1);
+                ::apply( yCol, selection1);
         /*auto select2 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
                 ::apply( col, selection2);*/
-        auto select2 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
-            persistent_ptr<MultiValTreeIndex>, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
-                ::apply( zTree, selection2);
+        auto select2 = my_select_wit_t<equal, ps, uncompr_f, uncompr_f>
+                ::apply( zCol, selection1);
+        //auto select2 = index_select_wit_t<std::equal_to, uncompr_f, uncompr_f,
+         //   persistent_ptr<MultiValTreeIndex>, persistent_ptr<NodeBucketList<uint64_t, OSP_SIZE>>>
+          //      ::apply( zTree, selection2);
         auto intersect = intersect_sorted<ps, uncompr_f, uncompr_f, uncompr_f >(select1, select2);
 
-        numa_run_on_node(runnode2);
+        //numa_run_on_node(runnode2);
         auto projection = my_project_wit_t<ps, uncompr_f, uncompr_f, uncompr_f >::apply(xCol, intersect);
         auto res = agg_sum<ps, uncompr_f>(projection);
 
@@ -233,10 +235,10 @@ public:
     using TempColPtr = std::unique_ptr<const column<uncompr_f>>;
     void main() {
         //auto repl_mgr = ReplicationManager::getInstance();
-        auto initializer = RootInitializer::getInstance();
+        auto & initializer = RootInitializer::getInstance();
         auto node_number = initializer.getNumaNodeCount();
 
-        std::cout << "Column Size in Tuples,Measure Unit,Selectivity,Volatile column,Persistent column,Persistent Tree,Persistent Hashmap,Persistent skiplist" << std::endl;
+        std::cout << "Column Size in Tuples,Measure Unit,Selectivity_y,Volatile column,Persistent column,Persistent Tree,Persistent Hashmap,Persistent skiplist,Selectivity_x" << std::endl;
         auto zStatus = repl_mgr.getStatus(RELATION, TABLE, Z);
         auto yStatus = repl_mgr.getStatus(RELATION, TABLE, Y);
         auto xStatus = repl_mgr.getStatus(RELATION, TABLE, X);
@@ -254,30 +256,34 @@ public:
             auto yPCol = yStatus->getPersistentColumn(node)->convert();
             auto yVCol = yStatus->getVColumn(node);
 
-            const uint64_t val2 = 2;
+            //const uint64_t val2 = 2;
 
-            for (uint64_t iterations = 0; iterations < 20; iterations++) {
-                for (size_t val = 1; val < MAX_SEL_Y; val++) {
-                    printColumnSize();
-                    comma();
-                    printUnit();
-                    comma();
-                    printSelectivity(yTree, val);
-                    comma();
-                    printNode(node);
-                    comma();
-                    runCol  (xCol, yVCol, zTree, val, val2, 0, 0);
-                    comma();
-                    runCol  (xCol, yPCol, zTree, val, val2, 0, 0);
-                    comma();
-                    runIndex(xCol, yTree, zTree, val, val2, 0, 0); 
-                    comma();
-                    runIndex(xCol, yHash, zTree, val, val2, 0, 0);
-                    comma();
-                    runIndex(xCol, ySkip, zTree, val, val2, 0, 0);
-                    comma();
-                    runCol  (xCol, yPCol, zPCol, val, val2, 0, 0);
-                    nextCsvRow();
+            for (uint64_t val2 = 2; val2 < 5; val2++) {
+                for (uint64_t iterations = 0; iterations < 15; iterations++) {
+                    for (size_t val = 1; val < MAX_SEL_Y; val++) {
+                        printColumnSize();
+                        comma();
+                        printUnit();
+                        comma();
+                        printSelectivity(yTree, val);
+                        comma();
+                        printNode(node);
+                        comma();
+                        runCol  (xCol, yVCol, zTree, val, val2, 0);
+                        comma();
+                        runCol  (xCol, yPCol, zTree, val, val2, 0);
+                        comma();
+                        runIndex(xCol, yTree, zTree, val, val2, 0); 
+                        comma();
+                        runIndex(xCol, yHash, zTree, val, val2, 0);
+                        comma();
+                        runIndex(xCol, ySkip, zTree, val, val2, 0);
+                        comma();
+                        printSelectivity(zTree, val2);
+                        //comma();
+                        //runCol  (xCol, yPCol, zPCol, val, val2, 0, 0);
+                        nextCsvRow();
+                    }
                 }
             }
 
@@ -287,7 +293,7 @@ public:
         }
 
         // Mixed node workload
-        auto xCol = xStatus->getPersistentColumn(0)->convert();
+        /*auto xCol = xStatus->getPersistentColumn(0)->convert();
         auto zTree = zStatus->getMultiValTreeIndex(1);
 
         auto yTree = yStatus->getMultiValTreeIndex(1);
@@ -322,7 +328,7 @@ public:
         }
 
         delete xCol;
-        delete yPCol;
+        delete yPCol;*/
         
     }
 
@@ -330,8 +336,8 @@ public:
 
 int main( void ) {
     // Setup phase: figure out node configuration
-    auto initializer = RootInitializer::getInstance();
-    ReplicationManager::getInstance();
+    auto & initializer = RootInitializer::getInstance();
+    //ReplicationManager::getInstance();
 
     if ( !initializer.isNuma() ) {
         trace_l(T_EXIT, "Current setup does not support NUMA, exiting...");
